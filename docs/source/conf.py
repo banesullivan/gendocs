425691d9e48131ee8182d9a12c1553e427c42eb3
Bane Sullivan <banesullivan@gmail.com>
2018-08-08 02:23:12 -0600

    Bump version: 0.0.2 → 0.1.0rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,6 +95,10 @@ pygments_style = 'sphinx'
 #
 html_theme = "sphinx_rtd_theme"
 
+html_context = {
+    "display_github": False,
+}
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
