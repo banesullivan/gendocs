7a5e2243445c28cd28a96d699d9899c1370a098d
Bane Sullivan <banesullivan@gmail.com>
2018-08-08 13:24:54 -0600

    Bump version: 0.2.0 → 0.2.1

--- gendocs/__init__.py
+++ gendocs/__init__.py
@@ -6,6 +6,6 @@ from .generator import *
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __displayname__ = 'gendocs'
 __name__ = 'gendocs'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import setuptools
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 with open("README.md", "r") as f:
     long_description = f.read()
