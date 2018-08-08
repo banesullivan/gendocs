572cd97d85e7d304ac7949a10a2283c52b985e65
Bane Sullivan <banesullivan@gmail.com>
2018-08-08 03:37:43 -0600

    Bump version: 0.1.0 → 0.1.1

--- gendocs/__init__.py
+++ gendocs/__init__.py
@@ -3,6 +3,6 @@ from .generator import *
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __displayname__ = 'gendocs'
 __name__ = 'gendocs'
